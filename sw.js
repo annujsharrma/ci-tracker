c8a2ca35f4551d560670418922f5674260d14904
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-20 02:00:54 +0530

    Add Email Draft generator with 5 templates (approval, extension, foreign-patient intimation, visa invitation, docs request)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v15';
+const CACHE = 'meditrack-v16';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
