6b90e331d02838095b426b768504d99d13ef16d2
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-20 01:22:58 +0530

    Fix untappable toast Undo; global stat cards; persist active tab across refresh; header eyebrow to SGRH

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v14';
+const CACHE = 'meditrack-v15';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
