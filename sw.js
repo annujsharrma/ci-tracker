92c9214d81f74e725bfadcdbf06c3353b4b6cc22
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-23 10:41:54 +0530

    Remove placeholder examples from Draft templates (blank fields, keep functional defaults)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v22';
+const CACHE = 'meditrack-v23';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
