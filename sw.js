924627129a1bdeb8c5f9f719980389d735756392
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-23 10:47:46 +0530

    Clear VIL signatory doctor/designation defaults (blank in draft)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v23';
+const CACHE = 'meditrack-v24';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
