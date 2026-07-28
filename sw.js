fc8dcb880fe840f40ae32ec1acb201e402c9e959
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-28 10:30:35 +0530

    Rename import menu item to Import

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v26';
+const CACHE = 'meditrack-v27';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
