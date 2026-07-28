2c93b11c855714be1d7f25c44e6434c2ff7e6941
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-28 10:29:09 +0530

    Show app version in reference menu to diagnose stale caches

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v25';
+const CACHE = 'meditrack-v26';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
