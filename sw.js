df78aae75fd792ceae884c4b6ea5501811a897ea
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-28 10:21:00 +0530

    Add one-time import of 21 current patients from July 2026 deadline sheet

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v24';
+const CACHE = 'meditrack-v25';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
