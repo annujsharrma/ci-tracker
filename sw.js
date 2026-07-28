725661ea3fcfa8d52bf5d2866994f7eef5256467
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-28 23:02:47 +0530

    Scan opens Adobe Scan (no Play Store), Mail opens Gmail inbox, with in-app fallbacks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v28';
+const CACHE = 'meditrack-v29';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
