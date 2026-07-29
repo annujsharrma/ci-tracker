d551067d6cd8fea9c3ebefe6f8fc95769736e942
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-29 11:11:44 +0530

    New MediTrack wordmark logo; brand-matched splash background

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v33';
+const CACHE = 'meditrack-v34';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
