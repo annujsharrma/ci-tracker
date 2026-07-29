7000dc57d652cd6ddcc71162bc78e48f82f44ea2
annujsharrma <o.anuj.sharma2002@gmail.com>
2026-07-29 10:40:30 +0530

    Clarify bulk add has no patient limit; add live row counter; strengthen chatbot prompt against truncation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meditrack-v32';
+const CACHE = 'meditrack-v33';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
